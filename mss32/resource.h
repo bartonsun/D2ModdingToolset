7d0e39f8f29b42cb300abbbfedbe2957c73e277f
Stanislav Egorov <unven@mail.ru>
2024-07-28 00:53:32 +0300

    Embed default lobby dialogs in mss32 resources

--- mss32/resource.h
+++ mss32/resource.h
@@ -1,14 +1,19 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by mss32.rc
+//
+#define DLG_CUSTOM_LOBBY 101
+#define DLG_ROOM_PASSWORD 102
+#define DLG_REGISTER_ACCOUNT 103
+#define DLG_LOGIN_ACCOUNT 104
 
-// Следующие стандартные значения для новых объектов
-// 
+// Next default values for new objects
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_RESOURCE_VALUE 102
+#define _APS_NEXT_COMMAND_VALUE 40001
+#define _APS_NEXT_CONTROL_VALUE 1001
+#define _APS_NEXT_SYMED_VALUE 101
 #endif
 #endif
