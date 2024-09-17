bead5f869a3d229bc8f2b103ce798131df94f032
Stanislav Egorov <unven@mail.ru>
2024-09-17 15:21:30 +0300

    Move detours.lib with pdb to common Lib directory

--- mss32/src/main.cpp
+++ mss32/src/main.cpp
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma comment(lib, "detours.lib")
+#pragma comment(lib, "Lib/detours.lib")
 
 #include "customaibattle.h"
 #include "customattacks.h"
